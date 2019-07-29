2436ee8c5d370e3e1aca71d5fc28870cc1190d89
Facundo Petre <fpetre@altimetrik.com>
2019-07-29 12:27:46 -0300

    2 review

---
+++ src/interfaces/index.ts
@@ -0,0 +1,26 @@
+export interface calendarState {
+  currentDate: number;
+  currentDateMin: number;
+  selectedDayMini: number;
+  currentDayId: number;
+  calendarMode: string;
+  currentDayIdMin: number;
+  calendarModeMin: string;
+  tasks: task[];
+}
+
+export interface action {
+  type?: string;
+  payload?: any;
+}
+
+export interface day {
+  timeStamp: number;
+  disabled: boolean;
+}
+
+export interface task {
+  title: string;
+  day: day;
+  time: Date;
+}
