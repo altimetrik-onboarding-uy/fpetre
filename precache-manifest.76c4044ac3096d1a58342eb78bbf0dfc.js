9931fdc8c1780fde18b0d519e7006ab043bfe0f6
Facundo Petre <fpetre@altimetrik.com>
2019-07-29 11:21:30 -0300

    gh-pages (second attempt)

---
+++ precache-manifest.76c4044ac3096d1a58342eb78bbf0dfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71251496ecae0adc763c873503bcc98b",
+    "url": "/fpetre/index.html"
+  },
+  {
+    "revision": "d83ae510593674985d12",
+    "url": "/fpetre/static/css/2.2687cd84.chunk.css"
+  },
+  {
+    "revision": "c0a20a7b48aacde1bd6a",
+    "url": "/fpetre/static/css/main.fd1d1684.chunk.css"
+  },
+  {
+    "revision": "d83ae510593674985d12",
+    "url": "/fpetre/static/js/2.06abf194.chunk.js"
+  },
+  {
+    "revision": "c0a20a7b48aacde1bd6a",
+    "url": "/fpetre/static/js/main.85d5d4da.chunk.js"
+  },
+  {
+    "revision": "ebe674c2c398b93d6529",
+    "url": "/fpetre/static/js/runtime~main.ca870218.js"
+  }
+]);
